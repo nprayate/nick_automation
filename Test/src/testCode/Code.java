dc3333d0d678a7a97adf1a1d4d20305981d760cc
Nikhil Rayate <nrayate693@gmail.com>
2025-05-15 16:50:34 +0530

    nick commit

--- Test/src/testCode/Code.java
+++ Test/src/testCode/Code.java
@@ -5,6 +5,8 @@ import java.security.DomainCombiner;
 public class Code {
 	public static void main(String[] args) {
 		System.err.println("test");
+		System.out.println("learning git hub command");
+		System.out.println("test the latest changes");
 	}
 
 }
